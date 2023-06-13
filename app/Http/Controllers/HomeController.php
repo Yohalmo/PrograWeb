6b526c1e136a17552f47df640c1f519cf0cf7288
Yohalmo <yohalmo@guanasoft.com>
2023-06-12 18:01:29 -0600

    limit landing page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,10 +11,12 @@ class HomeController extends Controller
 {
     public function LandingPage(){
 
-        $productos = Producto::all();
+        $productos = Producto::limit(6)->orderby(DB::raw('rand()'))->get();
+
         $categorias = Categoria::whereNotNull('IdProducto')->where('EstadoCategoria', 1)
             ->leftjoin('categorias_producto as cp', 'cp.IdCategoria', 'categorias.IdCategoria')
-            ->groupBy('categorias.IdCategoria')->get(['categorias.*', DB::raw('count(IdProducto) as productos')]);
+            ->groupBy('categorias.IdCategoria')->orderby(DB::raw('rand()'))->limit(3)
+            ->get(['categorias.*', DB::raw('count(IdProducto) as productos')]);
 
         return view('home.landing-page', compact('productos', 'categorias'));
     }
